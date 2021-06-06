aaa0acfe7a2feedded744eb02744c4b62dd4b543
BurairAbbas <65166629+BurairAbbas@users.noreply.github.com>
2021-06-06 09:39:44 +0500

    Add function DecideWinner

--- Static/js/app.js
+++ Static/js/app.js
@@ -41,3 +41,21 @@ function NumberToChoice(number) {
     return ["rock", "paper", "scissor"][number];
     // ["rock", "paper", "scissor"][0] gives rock, [1] gives paper, [2] gives scissor. More easy and short solution by clever 
 }
+
+
+function DecideWinner(humanChoice, botChoice) {
+    // we can go your if and else statements but it need alot of work. So, we use datastrructure which is suited for us.
+
+    var rpsDatabase = {
+        "rock": { "scissor": 1, "rock": 0.5, "paper": 0 }, // scissor u win, rock u draw, paper u loss.
+        "paper": { "rock": 1, "paper": 0.5, "scissor": 0 },
+        "scissor": { "paper": 1, "scissor": 0.5, "rock": 0 }
+    }
+
+    var yourScore = rpsDatabase[humanChoice][botChoice]; // your choice scissor and bot is paper return 1 u win
+    var botScore = rpsDatabase[botChoice][humanChoice];
+
+    return [yourScore, botScore];
+}
+
+
